35f5ae9e3c25db15ed9a897c34d27b929aed266b
kazuare <kazuare@mail.ru>
2017-11-05 03:12:01 +0300

    fix2

--- deploy.cmd
+++ deploy.cmd
@@ -154,7 +154,7 @@ popd
 :: 7. Run tests
 IF EXIST "%DEPLOYMENT_SOURCE%\tests.py" (
   echo RUNNING TESTS
-  %PYTHON_EXE% tests.py
+  env\scripts\python tests.py
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
